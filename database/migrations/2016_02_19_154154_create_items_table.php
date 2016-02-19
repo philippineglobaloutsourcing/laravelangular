b52868208a4ff2c8a427235519295c75fc0bb9a0
philippineglobaloutsourcing <philippineoutsourcing@gmail.com>
2016-02-20 00:20:36 +0800

    finished tutorial on Angular and Laravel 5.2

---
+++ database/migrations/2016_02_19_154154_create_items_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+   public function up()
+   {
+       Schema::create('items', function (Blueprint $table) {
+           $table->increments('id');
+           $table->string('title');
+           $table->text('description');
+           $table->timestamps();
+       });
+
+   }
+   public function down()
+   {
+       Schema::drop("items");
+   }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Laravel 5.2 and Angular JS Tutorial
+
+
+* note: I changed the ItemController.* to ItemsController.*
+
+
+Now build something awesome :)
